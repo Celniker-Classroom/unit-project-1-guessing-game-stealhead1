cfe5aaed1aa38d275dcf0954765b7a11ebd39ab4
Tymur Chernykov <127988729+stealhead1@users.noreply.github.com>
2026-04-07 20:31:28 +0000

    Got proximity to work and integrated names into messages

--- script.js
+++ script.js
@@ -3,23 +3,35 @@ let range;
 let targetNumber;
 let wins;
 let pName;
+let guess
 //shorthand, got tired of typing document.getElementById
 function getElement(id){
     return document.getElementById(id);
 }
 //gets name of player
 function getName(){
-    let temp = prompt("Enter your name")
+    let temp = prompt("Enter your name");
     //makes first letter uppercase and the rest lowercase
     pName = temp[0].toUpperCase() + temp.toLowerCase().slice(1,temp.length);
 }
-
+//checks proximity of guess to answer
+function hotOrCold(){
+    if(Math.abs(guess - targetNumber) <= 2){
+        return "hot"
+    }
+    else if (Math.abs(guess-targetNumber) <=5){
+        return "warm"
+    }
+    else if(Math.abs(guess - targetNumber) > 5){
+        return "cold"
+    }
+}
 //gets the target number and enables
 function play(){
     //gets value so we know range
     range = document.querySelector('input[name = "level"]:checked').value;
     //disables all radio buttons
-    let radioButtons = document.getElementsByName("level")
+    let radioButtons = document.getElementsByName("level");
     for(i = 0; i<radioButtons.length;i++){
         radioButtons[i].disabled = true;
     }
@@ -34,17 +46,18 @@ function play(){
 }
 function makeGuess(){
     //gets guess from text field
-    var guess = getElement("guess").value;
+    guess = getElement("guess").value;
     if(parseInt(guess) == targetNumber){
         wins ++;
-        getElement("msg").innerHTML = "Correct";
+        getElement("msg").innerHTML = "Correct, " + pName;
         getElement("guessBtn").disabled = true;
     }
     else if(parseInt(guess) > targetNumber){
-        getElement("msg").innerHTML = "High";
+        getElement("msg").innerHTML = "Too high " + pName + ", and you're " + hotOrCold();
+
     }
     else if(parseInt(guess) < targetNumber){
-        getElement("msg").innerHTML = "Low";
+        getElement("msg").innerHTML = "Too low" + pName + ", and you're " + hotOrCold();
     }
 }
 getElement("playBtn").addEventListener("click", play);
